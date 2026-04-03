88589cabc8414f478435f5d6401b8443db8f3d17
Lobbo <153131312+Cringemasteruwu@users.noreply.github.com>
2026-04-03 09:31:36 +0200

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,32 @@
+const CACHE = 'bowl-v4';
+const ASSETS = ['./index.html', './bowl-mascot.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  const isAsset = ASSETS.some(a => url.pathname.endsWith(a.replace('./','')));
+  if (!isAsset) return;
+  e.respondWith(
+    caches.open(CACHE).then(cache =>
+      fetch(e.request).then(res => {
+        cache.put(e.request, res.clone());
+        return res;
+      }).catch(() => cache.match(e.request))
+    )
+  );
+});
